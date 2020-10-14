67a8a82d1709d3f8d88dbd8682567932e961ac97
kwon <kwon.yh@altsoft.kr>
2020-10-14 16:09:29 +0900

    회원가입 프론트 작성 완료
    회원가입 프론트 작성 완료

--- blog/blog-frontend/src/pages/PostListPage.js
+++ blog/blog-frontend/src/pages/PostListPage.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import Button from '../components/common/Button';
+import HeadereContainer from '../containers/common/HeadereContainer';
 
 const PostListPage = () => {
     return (
-        <div>
-            <Button>버튼</Button>
-        </div>
+        <>
+            <HeadereContainer />
+            <div>안녕하세요.</div>
+        </>
     );
 };
 
